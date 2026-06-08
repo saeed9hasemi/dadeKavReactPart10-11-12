ed0a9737d369669ef2ff4d789d9df47f9d1a8f84
saeed <svenewacc@gmail.com>
2026-06-08 14:22:49 +0430

    build success browserRouter fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dadeKavReactPart10-11-12">
       <App />
     </BrowserRouter>
   </StrictMode>,
